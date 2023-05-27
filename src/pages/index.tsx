f0f36e5f729e06671a477fd8483611e55df64141
Jason <json.barba@protonmail.com>
2023-05-27 12:18:45 +0800

    chore: improve main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,52 @@ import { ArrowRightIcon } from '@radix-ui/react-icons';
 import { useAutoAnimate } from '@formkit/auto-animate/react'
 
 export default function Home() {
+  return (
+    <>
+      <NextSeo
+        title="Jason Barba"
+        description="Website of Jason Barba, here is where you can see details of his commercial experience as a web engineer/developer"
+      />
+      <div className="m-4">
+        <HeaderAvatar />
+        <div className="md:px-32 lg:px-80">
+          <AboutSection />
+          <GithubSection />
+          <ContractSection />
+        </div>
+      </div>
+    </>
+  );
+}
+
+function AboutSection() {
+  const currentYear = new Date().getFullYear();
+  return (
+    <section className="flex flex-col justify-center mt-8">
+      <h1 className="text-4xl leading-[50px] font-semibold tracking-wider text-[#2c3136] dark:text-white">About</h1>
+      <p className="max-w-4xl text-lg text-[#2c3136] dark:text-white font-medium leading-8 mt-4 tracking-[0.01em]">
+        <span className="font-semibold">
+          Jason Joseph Barba</span> is a software engineer mainly focus on the web frontend development side of things to help build web-based software solutions and services.
+          He spent over {currentYear - 2017} years working with various tech startups, mid-size organizations and even some larger ones. He likes to work on projects
+          that make our lives a bit better and easier.
+      </p>
+    </section>
+  );
+}
+
+function GithubSection() {
   const [parent] = useAutoAnimate();
+  const currentYear = new Date().getFullYear();
+  return (
+    <section ref={parent} className="space-y-4 mt-8">
+      <h1 className="text-4xl leading-[50px] font-semibold tracking-wider text-[#2c3136] dark:text-white">Github</h1>
+      <span className="text-sm text-[#2c3136] dark:text-white mb-4">This year&apos;s ({currentYear}) code contribution</span>
+      <GithubContributionMap />
+    </section>
+  );
+}
+
+function ContractSection() {
   const workList = [
     {
       imageSource: videocom,
@@ -69,77 +114,50 @@ export default function Home() {
     period: '2017 - 2019',
     highlights: [
       'Created websites, landing pages and web scrapers as a freelancer',
-      'Worked with a financial startup',
+      'Worked with a fintech startup',
       'Worked for a tech agency',
-      'Worked at a bank (Unionbank of the Philippines)'
+      'Worked at one of the top retail banks'
     ],
     technology: ['PHP', 'JavaScipt', 'TypeScript', 'React', 'Node', 'IBM API Connect', 'Firebase', "Git"]
   }
 
-  const currentYear = new Date().getFullYear();
-
   return (
-    <>
-      <NextSeo
-        title="Jason Barba"
-        description="Website of Jason Barba, here is where you can see details of his commercial experience as a web engineer/developer"
-      />
-      <div className="m-4">
-        <HeaderAvatar />
-        <div className="md:px-32 lg:px-80">
-          <section className="flex flex-col justify-center mt-8">
-            <h1 className="text-4xl leading-[50px] font-semibold tracking-wider text-[#2c3136] dark:text-white">About</h1>
-            <p className="max-w-4xl text-lg text-[#2c3136] dark:text-white font-medium leading-8 mt-4 tracking-[0.01em]">
-              <span className="font-semibold">
-                Jason Joseph Barba</span> is a software engineer mainly focus on the web frontend development side of things to help build software services that just make sense. 
-                He spent over {currentYear - 2017} years working with various tech startups, mid-size organizations and even some larger ones. He likes to work on things
-                that make our lives on the internet a little bit cleaner and easier.
-            </p>
-          </section>
-          <section ref={parent} className="space-y-4 mt-8">
-            <h1 className="text-4xl leading-[50px] font-semibold tracking-wider text-[#2c3136] dark:text-white">Github</h1>
-            <span className="text-sm text-[#2c3136] dark:text-white mb-4">This year&apos;s ({currentYear}) code contribution</span>
-            <GithubContributionMap />
-          </section>
-          <section className="flex flex-col mt-8">
-            <h1 className="text-4xl leading-[50px] font-semibold tracking-wider text-[#2c3136] dark:text-white">Contracts</h1>
-            <span className="text-sm text-[#2c3136] dark:text-white mb-4">List of commercial experiences</span>
-            <div className="grid grid-cols-1 gap-y-4 mt-2">
-              {workList.map((item, index) => (
-                <Fragment key={index}>
-                  <WorkCard
-                    imageSource={item.imageSource}
-                    name={item.name}
-                    designation={item.designation}
-                    period={item.period}
-                    highlights={
-                      item.highlights?.map((exp, i) => (
-                        <ul className="flex items-start" key={`h-${i}`}>
-                          <div><ArrowRightIcon className="-ml-4 mr-1 mt-[3px]"/></div><li>{exp}</li>
-                        </ul>
-                      ))
-                    }
-                    technology={item.technology}
-                  />
-                </Fragment>
-              ))}
-              <WorkCardGeneral 
-                name={legacyWork.name}
-                designation={legacyWork.designation}
-                period={legacyWork.period}
-                highlights={
-                  legacyWork.highlights?.map((exp, index) => (
-                    <ul className="flex items-start" key={index}>
-                      <div><ArrowRightIcon className="-ml-4 mr-1 mt-[3px]"/></div><li>{exp}</li>
-                    </ul>
-                  ))
-                }
-                technology={legacyWork.technology}
-              />
-            </div>
-          </section>
-        </div>
+    <section className="space-y-4 mt-8">
+      <h1 className="text-4xl leading-[50px] font-semibold tracking-wider text-[#2c3136] dark:text-white">Contracts</h1>
+      <span className="text-sm text-[#2c3136] dark:text-white mb-4">List of commercial experiences</span>
+      <div className="grid grid-cols-1 gap-y-4 mt-2">
+        {workList.map((item, index) => (
+          <Fragment key={index}>
+            <WorkCard
+              imageSource={item.imageSource}
+              name={item.name}
+              designation={item.designation}
+              period={item.period}
+              highlights={
+                item.highlights?.map((exp, i) => (
+                  <ul className="flex items-start" key={`h-${i}`}>
+                    <div><ArrowRightIcon className="-ml-4 mr-1 mt-[3px]"/></div><li>{exp}</li>
+                  </ul>
+                ))
+              }
+              technology={item.technology}
+            />
+          </Fragment>
+        ))}
+        <WorkCardGeneral
+          name={legacyWork.name}
+          designation={legacyWork.designation}
+          period={legacyWork.period}
+          highlights={
+            legacyWork.highlights?.map((exp, index) => (
+              <ul className="flex items-start" key={index}>
+                <div><ArrowRightIcon className="-ml-4 mr-1 mt-[3px]"/></div><li>{exp}</li>
+              </ul>
+            ))
+          }
+          technology={legacyWork.technology}
+        />
       </div>
-    </>
+    </section>
   );
 }
