5c78685bbf9a2ca1c8f7a8d8706a1957945da0e4
Jason <json.barba@protonmail.com>
2023-05-31 22:44:02 +0800

    feat: add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Noto_Sans } from 'next/font/google';
 import { ThemeProvider } from 'next-themes'
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Noto_Sans({
   fallback: ["-apple-system", "BlinkMacSystemFont", "Segoe UI", "Helvetica", "Arial", "sans-serif"],
@@ -14,6 +15,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <main className={inter.className}>
       <ThemeProvider attribute="class">
         <Component {...pageProps} />
+        <Analytics />
       </ThemeProvider>
     </main>
   );
