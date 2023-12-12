82b834a9868ee01b8911b90be0be2eb44bef1cfa
Jason <me@jsonbarba.com>
2023-12-12 10:42:41 +0800

    chore(insights): updated vercel speed insights

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Noto_Sans } from 'next/font/google';
 import { ThemeProvider } from 'next-themes'
-import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { DefaultSeo } from 'next-seo';
 import Head from 'next/head';
 
@@ -14,22 +14,24 @@ const inter = Noto_Sans({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={inter.className}>
-      <ThemeProvider attribute="class">
-        <DefaultSeo
-          defaultTitle="Jason Barba"
-          description="Have a nice day!"
-        />
-        <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=3, viewport-fit=cover"
-        />
-          <meta name="google-site-verification" content="eWe6gVI-N_FNp6_N2Kgegk3bWx-jkoKv028MeG1fEK4" />
-        </Head>
-        <Component {...pageProps} />
-        <Analytics />
-      </ThemeProvider>
-    </main>
+    <>
+      <main className={inter.className}>
+        <ThemeProvider attribute="class">
+          <DefaultSeo
+            defaultTitle="Jason Barba"
+            description="Have a nice day!"
+          />
+          <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, maximum-scale=3, viewport-fit=cover"
+          />
+            <meta name="google-site-verification" content="eWe6gVI-N_FNp6_N2Kgegk3bWx-jkoKv028MeG1fEK4" />
+          </Head>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </main>
+      <SpeedInsights />
+    </>
   );
 }
