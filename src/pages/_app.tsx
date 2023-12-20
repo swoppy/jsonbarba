4c3cc2967cbd38bf2c3d5fd69e1b144349b90e25
Jason <me@jsonbarba.com>
2023-12-20 11:26:07 +0800

    fix(_app): improve _app structure

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,22 +14,22 @@ const inter = Noto_Sans({
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <main className={inter.className}>
-        <ThemeProvider attribute="class">
-          <DefaultSeo
-            defaultTitle="Jason Barba"
-            description="Have a nice day!"
-          />
-          <Head>
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, maximum-scale=3, viewport-fit=cover"
-          />
-            <meta name="google-site-verification" content="eWe6gVI-N_FNp6_N2Kgegk3bWx-jkoKv028MeG1fEK4" />
-          </Head>
+      <DefaultSeo
+        defaultTitle="Jason Barba"
+        description="Have a nice day!"
+      />
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=3, viewport-fit=cover"
+        />
+        <meta name="google-site-verification" content="eWe6gVI-N_FNp6_N2Kgegk3bWx-jkoKv028MeG1fEK4" />
+      </Head>
+      <ThemeProvider attribute="class">
+        <main className={inter.className}>
           <Component {...pageProps} />
-        </ThemeProvider>
-      </main>
+        </main>
+      </ThemeProvider>
     </>
   );
 }
