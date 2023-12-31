ad96b285f428f9daa730ce756a4057910bff6dc6
Jason <me@jsonbarba.com>
2023-12-31 14:46:32 +0800

    fix(home): improved blur states

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -2,38 +2,77 @@ import Link from "next/link";
 import { useState } from "react";
 
 export function AboutSection({ currentYear }: { currentYear: number }) {
-  const [firstBlur, setFirstBlur] = useState(true);
-  const [secondBlur, setSecondBlur] = useState(true);
-  const [thirdBlur, setThirdBlur] = useState(true);
-  const [fourthBlur, setFourthBlur] = useState(true);
-  const [fifthBlur, setFifthBlur] = useState(true);
-  const [sixthBlur, setSixthBlur] = useState(true);
+  const [blur, setBlur] = useState({
+    first: true,
+    second: true,
+    third: true,
+    fourth: true,
+    fifth: true,
+    sixth: true
+  });
 
   const handleToggleForFirstBlur = () => {
-    if(!secondBlur && !firstBlur) {
-      setSecondBlur((previousState) => !previousState);
+    if(!blur.first && !blur.second) {
+      setBlur((previousStates) => {
+        return {
+        ...previousStates,
+        second: !previousStates.second,
+        third: !previousStates.third,
+        fourth: !previousStates.fourth,
+        }
+      });
     }
-    setFirstBlur((previousState) => !previousState);
+    setBlur((previousStates) => {
+      return {
+      ...previousStates,
+      first: !previousStates.first,
+      }
+    });
   }
 
   const handleToggleForSecondBlur = () => {
-    if (!firstBlur) setSecondBlur((previousState) => !previousState);
+    if (!blur.first) setBlur((previousStates) => {
+      return {
+      ...previousStates,
+      second: !previousStates.second,
+      }
+    });
   }
 
   const handleToggleForThirdBlur = () => {
-    if (!firstBlur && !secondBlur) setThirdBlur((previousState) => !previousState);
+    if (!blur.first && !blur.second) setBlur((previousStates) => {
+      return {
+      ...previousStates,
+      third: !previousStates.third,
+      }
+    });
   }
 
   const handleToggleForFourthBlur = () => {
-    setFourthBlur((previousState) => !previousState);
+    if (!blur.first && !blur.second && !blur.third) setBlur((previousStates) => {
+      return {
+      ...previousStates,
+      fourth: !previousStates.fourth,
+      }
+    });
   }
 
   const handleToggleForFifthBlur = () => {
-    setFifthBlur(false);
+    setBlur((previousStates) => {
+      return {
+      ...previousStates,
+      fifth: !previousStates.fifth,
+      }
+    });
   }
 
   const handleToggleForSixthBlur = () => {
-    setSixthBlur((previousState) => !previousState);
+    setBlur((previousStates) => {
+      return {
+      ...previousStates,
+      sixth: !previousStates.sixth,
+      }
+    });
   }
 
   return (
@@ -46,40 +85,40 @@ export function AboutSection({ currentYear }: { currentYear: number }) {
           </button>{' '}
         </span>{' '}
 
-        <span className={`inline ${firstBlur ? 'blur' : 'blur-0'}`}>
+        <span className={`inline ${blur.first ? 'blur' : 'blur-0'}`}>
           Barba, building things has been a constant fascination for me.{' '}
         </span>
         &nbsp;&nbsp;<span className="break-keep">
           I&#39;m based in Metro Manila Philippines.{' '}
           <button
-            className={`inline px-2 ${firstBlur ? 'blur cursor-text' : 'blur-0 cursor-pointer rounded-xl font-medium bg-slate-400 dark:text-dark'}`}
+            className={`inline px-2 ${blur.first ? 'blur cursor-text' : 'blur-0 cursor-pointer rounded-xl font-medium bg-slate-400 dark:text-dark'}`}
             onClick={handleToggleForSecondBlur}
           >
             Recently,
           </button>
         </span>{' '}
 
-        <span className={`inline ${secondBlur ? 'blur ' : 'blur-0'}`}>
+        <span className={`inline ${blur.second ? 'blur ' : 'blur-0'}`}>
           I&#39;ve been dabbling in writing — just checking it out and seeing where it takes me.{' '}
           <button
-            className={`inline px-2 ${secondBlur ? 'blur cursor-text' : 'blur-0 cursor-pointer rounded-xl font-medium bg-slate-400 dark:text-dark'}`}
+            className={`inline px-2 ${blur.second ? 'blur cursor-text' : 'blur-0 cursor-pointer rounded-xl font-medium bg-slate-400 dark:text-dark'}`}
             onClick={handleToggleForThirdBlur}
           >
             Moreover,
           </button>
         </span>{' '}
 
-        <span className={`inline ${thirdBlur ? 'blur' : 'blur-0'}`}>
+        <span className={`inline ${blur.third ? 'blur' : 'blur-0'}`}>
           my background is mostly around web app and UI development.{' '}<br/><br/>
           <button
-            className={`inline px-2 ${thirdBlur ? 'blur cursor-text' : 'blur-0 cursor-pointer font-medium bg-slate-400 dark:text-dark'}`}
+            className={`inline px-2 ${blur.third ? 'blur cursor-text' : 'blur-0 cursor-pointer font-medium bg-slate-400 dark:text-dark'}`}
             onClick={handleToggleForFourthBlur}
           >
             Over the last {currentYear - 2017} years,
           </button>{' '}
         </span>
 
-        <span className={`inline ${fourthBlur ? 'blur' : 'blur-0'}`}>
+        <span className={`inline ${blur.fourth ? 'blur' : 'blur-0'}`}>
           I&#39;ve navigated various professional landscapes, from startups to midsize and large organizations, with different capacities, gathering insights and expertise along the way.{' '}
         </span><br/><br/>
 
@@ -103,17 +142,17 @@ export function AboutSection({ currentYear }: { currentYear: number }) {
             href="https://asurion.com"
             rel="noopener noreferrer"
             target="_blank"
-            className={`inline underline decoration-dotted underline-offset-4 decoration-1 hover:decoration-indigo-400 ${fifthBlur ? 'blur' : 'blur-0'}`}
+            className={`inline underline decoration-dotted underline-offset-4 decoration-1 hover:decoration-indigo-400 ${blur.fifth ? 'blur' : 'blur-0'}`}
           >
             &#40;a tech insurance company based in Nashville&#41;
           </a>{' '}
-          <span className={`${fifthBlur ? 'blur' : 'blur-0'}`}>as a software engineer.</span>
+          <span className={`${blur.fifth ? 'blur' : 'blur-0'}`}>as a software engineer.</span>
         </span><br/><br/>
 
         <span>
           You can reach me at me@jsonbarba.com{' '}
           <button className="inline px-2 rounded-xl font-medium bg-slate-400 dark:text-dark" onClick={handleToggleForSixthBlur}>Also,</button>{' '}
-          <span className={`inline ${sixthBlur ? 'blur' : 'blur-0'}`}>I&#39;m on Github, LinkedIn and X.</span>
+          <span className={`inline ${blur.sixth ? 'blur' : 'blur-0'}`}>I&#39;m on Github, LinkedIn and X.</span>
         </span>
       </p>
     </section>
