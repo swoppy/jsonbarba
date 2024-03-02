e57f8d40f5b912d80202a329001dc1d1cd536125
Jason <me@jsonbarba.com>
2024-03-02 23:42:28 +0800

    feat(home): created pulsing animation for button emphasis

--- src/components/home/AboutSection.tsx
+++ src/components/home/AboutSection.tsx
@@ -1,11 +1,24 @@
 import Link from "next/link";
 import { useBlur } from "./useBlur";
 import { useHover } from "./useHover";
-import { serif } from "@/font/fonts";
+import { serif, sans } from "@/font/fonts";
+
+function PulsingDot({ tw }: { tw?: string }) {
+  return (
+    <span className={`absolute ${tw}`}>
+      <span className="relative flex h-3 w-3">
+        <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-gray-200 opacity-75"></span>
+        <span className="relative inline-flex rounded-full h-3 w-3 bg-gray-300"></span>
+      </span>
+    </span>
+  );
+}
 
 export function AboutSection({ currentYear }: { currentYear: number }) {
   const { states:blur, handlers:toggle  } = useBlur();
   const { states:hover, handlers:onMouse } = useHover();
+
+  // TODO: improve button focus on events
 
   return (
     <section className="flex flex-col justify-center mt-8 md:mt-0">
@@ -14,22 +27,25 @@ export function AboutSection({ currentYear }: { currentYear: number }) {
           Hey! I&#39;m{' '}
         </span>{' '}
         <button
-          className={`inline-flex rounded-xl bg-indigo-400 dark:text-dark ${serif.className}`}
+          className={`inline-flex relative rounded-xl bg-indigo-400 dark:text-dark ${serif.className}`}
           onClick={toggle.firstBlur}
           onMouseOver={() => onMouse.firstGroup({ event: 'over' })}
           onMouseOut={() => onMouse.firstGroup({ event: 'out'})}
         >
           <span className="mr-2 ml-2.5 font-medium">JASON</span>
+          {blur.JASON && <PulsingDot tw="-mt-1"/>}
         </button>{' '}
+        
 
-        <span className={`tracking-tight inline transition-all duration-400 ${blur.JASON ? 'blur select-none' : `blur-0 select-auto ${hover.first && 'text-indigo-300'}`}`}>
+        <span className={`tracking-tight inline transition-all duration-400 ${blur.JASON ? 'blur select-none ease-out' : `blur-0 select-auto ease-out ${hover.first && 'text-indigo-300'}`}`}>
           Barba, building things has been a constant fascination for me. My journey in the world of creation has been an exciting blend of curiosity, learning, and hands-on experience.{' '}
         </span><br /><br />
         <span className={`tracking-tight inline transition-all duration-500 ${(hover.first && !blur.JASON) && 'text-indigo-200'}`}>
           I&#39;m based in Metro Manila Philippines.{' '}
           <button
+            id="recently"
             className={`
-              inline px-2 w-[98px] md:w-[155px] ${serif.className}
+              inline relative px-2 w-[98px] md:w-[155px] ${serif.className}
               ${blur.JASON ? 'blur cursor-text' :
               'blur-0 cursor-pointer rounded-xl font-medium bg-slate-400 hover:bg-indigo-400 focus:bg-indigo-400 transition-colors duration-500 dark:text-dark'}
             `}
@@ -37,13 +53,15 @@ export function AboutSection({ currentYear }: { currentYear: number }) {
             onMouseOver={() => onMouse.secondGroup({ event: 'over' })}
             onMouseOut={() => onMouse.secondGroup({ event: 'out' })}
           >
-            Recently,
+            {(blur.RECENTLY && !blur.JASON) && <PulsingDot tw="-mt-1.5 left-0"/>}
+            <span>Recently,</span>
           </button>
         </span>{' '}
 
         <span className={`inline transition-all duration-500 ${blur.RECENTLY ? 'blur select-none' : `blur-0 selec-auto ${hover.second && 'text-indigo-300'}`}`}>
           I&#39;ve been dabbling in writing — just checking it out and seeing where it takes me.{' '}
           <button
+            id="moreover"
             className={`
               inline transition-all duration-500 px-2 w-[108px] md:w-[168px]
               ${serif.className} ${blur.RECENTLY ? 'blur select-none' :
@@ -53,7 +71,8 @@ export function AboutSection({ currentYear }: { currentYear: number }) {
             onMouseOver={() => onMouse.thirdGroup({ event: 'over' })}
             onMouseOut={() => onMouse.thirdGroup({ event: 'out' })}
           >
-            Moreover,
+            {(blur.MOREOVER && !blur.RECENTLY) && <PulsingDot tw="-mt-1.5 left-0"/>}
+            <span>Moreover,</span>
           </button>
         </span>{' '}
 
@@ -69,7 +88,8 @@ export function AboutSection({ currentYear }: { currentYear: number }) {
             onMouseOver={() => onMouse.fourthGroup({ event: 'over' })}
             onMouseOut={() => onMouse.fourthGroup({ event: 'out' })}
           >
-            Over the last {currentYear - 2017} years,
+            <span>Over the last {currentYear - 2017} years,</span>
+            {(blur.OVER_THE_LAST && !blur.MOREOVER) && <PulsingDot tw="-mt-1.5 left-0"/>}
           </button>{' '}
         </span>
 
@@ -87,12 +107,13 @@ export function AboutSection({ currentYear }: { currentYear: number }) {
           </Link> at{' '}
         </span>
         <button
-          className={`transition-all duration-500 w-[86px] md:w-[134px] inline px-2 rounded-xl font-medium bg-slate-400 hover:bg-indigo-400 focus:bg-indigo-400 dark:text-dark ${serif.className}`}
+          className={`transition-all duration-500 relative w-[86px] md:w-[134px] inline px-2 rounded-xl font-medium bg-slate-400 hover:bg-indigo-400 focus:bg-indigo-400 dark:text-dark ${serif.className}`}
           onClick={toggle.fifthBlur}
           onMouseOver={() => onMouse.fifthGroup({ event: 'over' })}
           onMouseOut={() => onMouse.fifthGroup({ event: 'out' })}
         >
-          Asurion
+          {(blur.ASURION && !blur.OVER_THE_LAST) && <PulsingDot tw="-mt-1.5 left-0"/>}
+          <span>Asurion</span>
         </button>{' '}
         <span>
           <a
@@ -121,12 +142,12 @@ export function AboutSection({ currentYear }: { currentYear: number }) {
             me@jsonbarba.com
           </a>{' '}
           <button
-            className={`transition-all duration-500 w-[60px] md:w-[86px] inline px-2 rounded-xl font-medium bg-slate-400 hover:bg-indigo-400 focus:bg-indigo-400 dark:text-dark ${serif.className}`}
+            className={`relative transition-all duration-500 w-[60px] md:w-[86px] inline px-2 rounded-xl font-medium bg-slate-400 hover:bg-indigo-400 focus:bg-indigo-400 dark:text-dark ${serif.className}`}
             onClick={toggle.sixthBlur}
             onMouseOver={() => onMouse.sixthGroup({ event: 'over' })}
             onMouseOut={() => onMouse.sixthGroup({ event: 'out' })}
           >
-            Also,
+            <span>Also,</span>
           </button>{' '}
           <span
             className={`inline transition-all duration-500 ${blur.ALSO ? 'blur select-none' : `blur-0 select-auto ${hover.sixth && 'text-indigo-300'}`}`}
