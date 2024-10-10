85dd12244a0ac890321d91f2a9471601f78d936a
Jason <me@jsonbarba.com>
2024-10-10 23:27:31 +0800

    writing: improve writing page - md to mdx - added writing entry: driving school

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -2,11 +2,13 @@ import { defineDocumentType, makeSource } from 'contentlayer/source-files'
 
 export const Post = defineDocumentType(() => ({
   name: 'Writing',
-  filePathPattern: `**/*.md`,
+  filePathPattern: `**/*.mdx`,
+  contentType: 'mdx',
   fields: {
     title: { type: 'string', required: true },
     date: { type: 'date', required: true },
     bodyPreview: { type: 'string', required: true },
+    image: { type: 'string', required: false },
   },
   computedFields: {
     url: { type: 'string', resolve: (post) => `/writing/${post._raw.flattenedPath}` },
--- src/pages/writing/[slug]/index.tsx
+++ src/pages/writing/[slug]/index.tsx
@@ -1,31 +1,63 @@
 import { format, parseISO } from 'date-fns';
-import { allWritings } from 'contentlayer/generated';
+import { allWritings, Writing } from 'contentlayer/generated';
+import { useMDXComponent } from 'next-contentlayer/hooks'
 import { useRouter } from 'next/router';
 import { MainLayout } from '@/components/MainLayout';
 import { NextSeo } from 'next-seo';
+import type { MDXComponents } from 'mdx/types'
+import Image from 'next/image';
+import { GetStaticPropsContext } from 'next';
 
-const Content = () => {
-  const router = useRouter();
-  const post = allWritings.find((post) => post._raw.flattenedPath === router?.query.slug);
+// TODO: switch contentlayer to velite https://velite.js.org/guide/using-mdx
 
-  if (!post) return null;
+const CustomMdxComponents: MDXComponents =  {
+  img: ({ src, alt }) =>  <Image  src={src as string} alt={alt as string} className="rounded-xl shadow-lg" width={456} height={456}/>
+}
+
+export async function getStaticPaths() {
+  // Get a list of valid writing paths.
+  const paths = allWritings.map((post) => ({
+    params: { slug: post._raw.flattenedPath },
+  }))
+
+  return { paths, fallback: false }
+}
+
+export async function getStaticProps(context: GetStaticPropsContext) {
+  // Find the post for the current page.
+  const writing = allWritings.find((post) => post._raw.flattenedPath === context.params?.slug)
+
+  // Return notFound if the post does not exist.
+  if (!writing) return { notFound: true }
+
+  // Return the post as page props.
+  return { props: { writing } }
+}
+
+const Content = ({ writing }: { writing: Writing }) => {
+  // const post = allWritings.find((post) => post._raw.flattenedPath === router?.query.slug) as Writing;
+  const MDXContent = useMDXComponent(writing?.body.code)
+
+  if (!writing) return null;
 
   return (
     <>
       <NextSeo
-        title={post.title}
-        description={post.bodyPreview}
+        title={writing.title}
+        description={writing.bodyPreview}
         themeColor="dark"
       />
       <MainLayout>
         <article className="py-8">
           <div className="mb-10">
-            <h1 className="text-3xl font-bold">{post.title}</h1>
-            <time dateTime={post.date} className="mb-1 text-base text-gray-400">
-              {format(parseISO(post.date), 'LLL d, yyyy')}
+            <h1 className="text-3xl font-bold">{writing.title}</h1>
+            <time dateTime={writing.date} className="mb-1 text-base text-gray-400">
+              {format(parseISO(writing.date), 'LLL d, yyyy')}
             </time>
           </div>
-          <div className="[&>*]:mb-3 [&>*:last-child]:mb-0 prose md:prose-lg dark:prose-invert" dangerouslySetInnerHTML={{ __html: post.body.html }} />
+          <div className="[&>*]:mb-3 [&>*:last-child]:mb-0 prose md:prose-lg dark:prose-invert">
+            <MDXContent components={CustomMdxComponents}/>
+          </div>
         </article>
       </MainLayout>
     </>
