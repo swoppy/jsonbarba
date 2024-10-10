7dba66277ce63d74defc9f84248ae6a540d79804
Jason <me@jsonbarba.com>
2024-10-10 19:19:53 +0800

    fix: fix mobile view for writing page

--- src/pages/writing/index.tsx
+++ src/pages/writing/index.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 
 function WritingPreview(post: Writing) {
   return (
-    <div className="flex justify-between mb-8">
+    <div className="flex flex-col md:justify-between md:flex-row mb-8">
       <div>
         <h3 className="mb-1">
           <Link href={post.url} className="transition-all duration-500 hover:text-indigo-400 text-base sm:text-lg">
@@ -16,7 +16,7 @@ function WritingPreview(post: Writing) {
         <span className="text-gray-400 font-light text-sm md:text-base">{post.bodyPreview}</span>
       </div>
       <div className="mt-1">
-        <time dateTime={post.date} className="text-gray-400 font-light text-sm md:text-base">
+        <time dateTime={post.date} className="text-gray-400 font-light text-xs md:text-base">
           {format(parseISO(post.date), 'LLL d, yyyy')}
         </time>
       </div>
@@ -36,7 +36,7 @@ const WritingPage = () => {
       <MainLayout>
         <div className="mt-8">
           <h1 className="font-semibold text-dark text-2xl sm:text-3xl dark:text-white">Writing</h1>
-          <p className="max-w-4xl text-dark dark:text-white mt-4 font-light leading-8 tracking-[0.01em] text-base sm:text-lg">Life experiences, learnings, values, brain dumps. <br/>Pretty much anything I can think of, and something worthy to share.</p>
+          <p className="max-w-4xl text-dark dark:text-white mt-4 font-light leading-8 tracking-[0.01em] text-base sm:text-lg">Life experiences, learnings, values, brain dumps. Pretty much anything I can think of, and something worthy to share.</p>
         </div>
         <div className="mt-12">
           {posts.map((post, idx) => (
